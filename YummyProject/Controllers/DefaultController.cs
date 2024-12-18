7d54f79ad20618c5482857aea4b6756bb4d6906e
Melisa Akkuş <melisa.akkus01@gmail.com>
2024-12-19 00:12:33 +0300

    Updates on UI

--- YummyProject/Controllers/DefaultController.cs
+++ YummyProject/Controllers/DefaultController.cs
@@ -26,5 +26,57 @@ namespace YummyProject.Controllers
             var values = context.Abouts.ToList();   
             return PartialView(values);
         }
+
+        public PartialViewResult DefaultService()
+        {
+            var values = context.Services.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult DefaultProduct() // eksik
+        {
+            var values = context.Products.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult DefaultTestimonial()
+        {
+            var values = context.Testimonials.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult DefaultEvent()
+        {
+            var values = context.Events.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult DefaultChef() // sosyal medya eksik
+        {
+            var values = context.Chefs.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult DefaultBooking()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult DefaultPhotoGallery() //bazı resimler değişecek
+        {
+            var values = context.PhotoGalleries.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult DefaultMessage()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult DefaultContact() //item.mapurl çalışmıyor
+        {
+            var values = context.ContactInfos.ToList();
+            return PartialView(values);
+        }
     }
 }
